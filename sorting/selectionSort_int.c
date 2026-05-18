b0ee17af5686c2ef2b3a99a4f152e28bda8226e0
Re Gomes <95058148+ReStyleGM@users.noreply.github.com>
2026-05-18 18:21:36 -0300

    Add files via upload

---
+++ sorting/selectionSort_int.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+void imprime(int *v, int n) {
+  int i;
+  for(i = 0; i<n; i++){
+    printf("%d ", v[i]);
+  }
+  printf("\n");
+}
+
+void ordena (int *v, int n){
+  int i, j, min, total = 0;
+  for(i = 0; i < n-1; i++){
+    min = i;
+    for(j = i+1; j < n; j++){ // procura o indice do i-esimo "menor" elemento
+      if(v[j] < v[min]){
+        min = j;
+      }
+      total += 1;
+    }
+    // ajusta i-esimo menor na posição i
+    int aux = v[i];
+    v[i] = v[min];
+    v[min] = aux;
+    // imprime(v, n); para pegar todos as ordenações
+  }
+  imprime(v, n);
+  printf("Total de IFs executados: %d", total);
+}
+
+int main()
+{
+    int *v, n, i;
+    scanf("%d", &n);
+    v = (int *) malloc(sizeof(int) * n);
+    if(v == NULL){
+      return 1;
+    }
+    for(i=0; i < n; i++) {
+      scanf("%d", &v[i]);
+    }
+    ordena(v, i);
+    free(v);
+}
